3950a6df9cd23e8c414ba31b04693c7f03926d21
Insori <82927307+Insori@users.noreply.github.com>
2022-04-04 10:17:57 +0900

    [ADD] 0404 내적 (#4)
    * [ADD] 0404 내적
    
    * [ADD] 0404 내적

---
+++ src/programmers/Solution6.java
@@ -0,0 +1,27 @@
+package programmers;
+import java.util.Scanner;
+
+public class Solution6 {
+    public int solution(int[] a, int[] b) {
+        int answer=0;
+        for(int i=0; i<a.length; i++) {
+            answer+=(a[i]*b[i]);
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        Solution6 s=new Solution6();
+        int len=scan.nextInt(); //배열 길이
+        int[] a=new int[len];
+        int[] b=new int[len];
+        for(int i=0; i<len; i++) {  //a 배열의 값 입력
+            a[i]=scan.nextInt();
+        }
+        for(int i=0; i<len; i++) {  //b 배열의 값 입력
+            b[i]=scan.nextInt();
+        }
+        System.out.println();
+        System.out.println(s.solution(a,b));
+    }
+}
